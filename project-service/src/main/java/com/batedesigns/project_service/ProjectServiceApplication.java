8bc0f2d5310781e772f180fa2788ba277cc9205a
nzivo <johnnnzivo@gmail.com>
2024-10-02 02:04:03 +0300

    Create and View Projects

---
+++ project-service/src/main/java/com/batedesigns/project_service/ProjectServiceApplication.java
@@ -0,0 +1,15 @@
+package com.batedesigns.project_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ProjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectServiceApplication.class, args);
+	}
+
+}
