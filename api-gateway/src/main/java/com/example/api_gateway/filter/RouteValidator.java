23888826d7911e25417bd208dcf7f91bd81db3a2
nzivo <johnnnzivo@gmail.com>
2024-10-03 04:09:37 +0300

    DNS error and authenticate token for all requests

---
+++ api-gateway/src/main/java/com/example/api_gateway/filter/RouteValidator.java
@@ -0,0 +1,21 @@
+package com.example.api_gateway.filter;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+    public static final List<String> openApiEndpoints = List.of(
+            "/auth/register",
+            "/auth/token",
+            "/eureka"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+}
